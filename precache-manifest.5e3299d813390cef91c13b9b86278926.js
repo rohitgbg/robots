843b6ad0595e0440902ea03494549662ad725788
Rohith GB <rohit.gb@preceptlabs.com>
2020-02-01 17:15:08 +0530

    Updates

---
+++ precache-manifest.5e3299d813390cef91c13b9b86278926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db68f15332849c1dbaa97fa4f5a95bf",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "ce520705d050e0b1ea38",
+    "url": "/robots/static/css/main.a8e69a61.chunk.css"
+  },
+  {
+    "revision": "9a95dfddb41d169be3f2",
+    "url": "/robots/static/js/2.e2fa5335.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robots/static/js/2.e2fa5335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce520705d050e0b1ea38",
+    "url": "/robots/static/js/main.b9c41fb6.chunk.js"
+  },
+  {
+    "revision": "1e2f306dd9fc79e0b3e4",
+    "url": "/robots/static/js/runtime-main.fa954eb3.js"
+  }
+]);
